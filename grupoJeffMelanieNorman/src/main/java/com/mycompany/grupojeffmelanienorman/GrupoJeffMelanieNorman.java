6e764284d3c1f5a6315905877115f7e4cf303c97
Jeffry Araya Ch <jeffry2811ach@gmail.com>
2023-12-06 17:35:12 -0600

    cosas

---
+++ grupoJeffMelanieNorman/src/main/java/com/mycompany/grupojeffmelanienorman/GrupoJeffMelanieNorman.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.grupojeffmelanienorman;
+
+/**
+ *
+ * @author XPC
+ */
+public class GrupoJeffMelanieNorman {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
