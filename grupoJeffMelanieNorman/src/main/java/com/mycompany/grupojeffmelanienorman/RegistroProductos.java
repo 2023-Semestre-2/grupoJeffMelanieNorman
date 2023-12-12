821dbe348e1bf77e6dbc4ceee80fe87b7622d0c9
Norman Sáenz A <saenznorman73@gmail.com>
2023-12-12 16:38:37 -0600

    Eliminar productos
    Pasé los productos a información quemada.

--- grupoJeffMelanieNorman/src/main/java/com/mycompany/grupojeffmelanienorman/RegistroProductos.java
+++ grupoJeffMelanieNorman/src/main/java/com/mycompany/grupojeffmelanienorman/RegistroProductos.java
@@ -9,9 +9,7 @@ import java.util.Scanner;
 
 public class RegistroProductos {
 
-    private JSONArray listaProductos;
     private JSONArray listaArticulos;
-    private int ultimoCodigoProducto;
     private int ultimoCodigoArticulo;
     private static final String FILE_PATH = "Productos.json";
 
@@ -25,55 +23,25 @@ public class RegistroProductos {
         try {
             FileReader reader = new FileReader(FILE_PATH);
             JSONObject obj = (JSONObject) parser.parse(reader);
-
-            // Verifica si existen las claves "Productos" y "Articulos"
-            if (obj.containsKey("Productos")) {
-                listaProductos = (JSONArray) obj.get("Productos");
-            } else {
-                listaProductos = new JSONArray(); // Inicializa si no existe
-            }
-
-            if (obj.containsKey("Articulos")) {
-                listaArticulos = (JSONArray) obj.get("Articulos");
-            } else {
-                listaArticulos = new JSONArray(); // Inicializa si no existe
-            }
-
-            actualizarUltimoCodigo();
+            listaArticulos = (JSONArray) obj.getOrDefault("Articulos", new JSONArray());
+            actualizarUltimoCodigo(); // Actualizar el último código después de cargar los datos
         } catch (Exception e) {
-            listaProductos = new JSONArray();
             listaArticulos = new JSONArray();
         }
     }
+
+
     private void actualizarUltimoCodigo() {
-        if (!listaProductos.isEmpty()) { // Se ejecuta solo si listaProductos tiene al menos un elemento
-            JSONObject ultimoProducto = (JSONObject) listaProductos.get(listaProductos.size() - 1);
-            ultimoCodigoProducto = ((Long) ultimoProducto.get("Codigo")).intValue();
-        }
         if (!listaArticulos.isEmpty()) {
             JSONObject ultimoArticulo = (JSONObject) listaArticulos.get(listaArticulos.size() - 1);
             ultimoCodigoArticulo = ((Long) ultimoArticulo.get("Codigo")).intValue();
         }
     }
 
-    public void agregarProducto() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese el nombre del producto:");
-        String nombre = scanner.nextLine();
 
-        ultimoCodigoProducto++;
 
-        JSONObject nuevoProducto = new JSONObject();
-        nuevoProducto.put("Codigo", ultimoCodigoProducto);
-        nuevoProducto.put("Nombre", nombre);
 
-        listaProductos.add(nuevoProducto);
-        guardarDatos();
-
-        System.out.println("Producto agregado con éxito: " + nuevoProducto);
-    }
-
-        public void agregarArticulo() {
+    public void agregarArticulo() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese el nombre del artículo:");
         String nombre = scanner.nextLine();
@@ -101,7 +69,7 @@ public class RegistroProductos {
         }
 
         ultimoCodigoArticulo++;
-        
+
         System.out.println("Ingrese la marca del artículo:");
         String marca = scanner.nextLine();
 
@@ -111,7 +79,7 @@ public class RegistroProductos {
         System.out.println("Ingrese el precio del artículo:");
         int precio = scanner.nextInt();
         scanner.nextLine();
-        
+
         JSONObject nuevoArticulo = new JSONObject();
         nuevoArticulo.put("Codigo", ultimoCodigoArticulo);
         nuevoArticulo.put("Nombre", nombre);
@@ -142,137 +110,142 @@ public class RegistroProductos {
     private void guardarDatos() {
         try (FileWriter file = new FileWriter(FILE_PATH)) {
             JSONObject obj = new JSONObject();
-            obj.put("Productos", listaProductos);
-            obj.put("Articulos", listaArticulos);
+            obj.put("Articulos", listaArticulos); // Guarda la lista de artículos
             file.write(obj.toJSONString());
             file.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
-    public void modificar(JSONObject objeto) {
-        Scanner scanner = new Scanner(System.in);
-        boolean esProducto = objeto.containsKey("Marca"); // Suponiendo que solo los artículos tienen 'Marca'
 
-        System.out.println("Modificando " + (esProducto ? "artículo" : "producto") + ": " + objeto);
+    
+    public void modificar(JSONObject articulo) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Modificando artículo: " + articulo);
         System.out.println("Ingrese los nuevos valores (deje en blanco para no cambiar):");
 
         // Nombre
-        System.out.println("Nombre (" + objeto.get("Nombre") + "): ");
+        System.out.println("Nombre (" + articulo.get("Nombre") + "): ");
         String nombre = scanner.nextLine();
         if (!nombre.isEmpty()) {
-            objeto.put("Nombre", nombre);
+            articulo.put("Nombre", nombre);
         }
-
-        // Marca y otros campos para artículos
-        if (esProducto) {
-            // Marca
-            System.out.println("Marca (" + objeto.get("Marca") + "): ");
-            String marca = scanner.nextLine();
-            if (!marca.isEmpty()) {
-                objeto.put("Marca", marca);
+        // Marca
+        System.out.println("Marca (" + articulo.get("Marca") + "): ");
+        String marca = scanner.nextLine();
+        if (!marca.isEmpty()) {
+            articulo.put("Marca", marca);
+        }
+        // Cantidad
+        System.out.println("Cantidad (" + articulo.get("Cantidad") + "): ");
+        String cantidadStr = scanner.nextLine();
+        if (!cantidadStr.isEmpty()) {
+            try {
+                int cantidad = Integer.parseInt(cantidadStr);
+                articulo.put("Cantidad", cantidad);
+            } catch (NumberFormatException e) {
+                System.out.println("Cantidad no válida, se mantiene el valor anterior.");
             }
-
-            // Cantidad
-            System.out.println("Cantidad (" + objeto.get("Cantidad") + "): ");
-            String cantidad = scanner.nextLine();
-            if (!cantidad.isEmpty()) {
-                objeto.put("Cantidad", Integer.parseInt(cantidad));
+        }
+        // Precio
+        System.out.println("Precio (" + articulo.get("Precio") + "): ");
+        String precioStr = scanner.nextLine();
+        if (!precioStr.isEmpty()) {
+            try {
+                int precio = Integer.parseInt(precioStr);
+                articulo.put("Precio", precio);
+            } catch (NumberFormatException e) {
+                System.out.println("Precio no válido, se mantiene el valor anterior.");
             }
-
-            // Precio
-            System.out.println("Precio (" + objeto.get("Precio") + "): ");
-            String precio = scanner.nextLine();
-            if (!precio.isEmpty()) {
-                objeto.put("Precio", Integer.parseInt(precio));
-            }
-
-            // Tipo
-            System.out.println("Tipo (" + objeto.get("Tipo") + "): ");
-            String tipo = scanner.nextLine();
-            if (!tipo.isEmpty()) {
-                objeto.put("Tipo", tipo);
-            }
-
-            // Tamaño
-            System.out.println("Tamaño (" + objeto.get("Tamaño") + "): ");
-            String tamaño = scanner.nextLine();
-            if (!tamaño.isEmpty()) {
-                objeto.put("Tamaño", tamaño);
-            }
+        }
+        // Tipo
+        System.out.println("Tipo (" + articulo.get("Tipo") + "): ");
+        String tipo = scanner.nextLine();
+        if (!tipo.isEmpty()) {
+            articulo.put("Tipo", tipo);
+        }
+        // Tamaño
+        System.out.println("Tamaño (" + articulo.get("Tamaño") + "): ");
+        String tamaño = scanner.nextLine();
+        if (!tamaño.isEmpty()) {
+            articulo.put("Tamaño", tamaño);
         }
 
         guardarDatos(); // Guardar los cambios en el archivo JSON
 
-        System.out.println("Modificación completada: " + objeto);
+        System.out.println("Modificación completada: " + articulo);
+    }
+
+    public void buscar() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Seleccione el criterio de búsqueda:");
+        System.out.println("1. Por Código");
+        System.out.println("2. Por Nombre");
+        int criterioBusqueda = scanner.nextInt();
+        scanner.nextLine(); // Consumir el salto de línea
+
+        JSONObject encontrado = null;
+
+        if (criterioBusqueda == 1) {
+            System.out.println("Ingrese el código del artículo:");
+            int codigo = scanner.nextInt();
+            scanner.nextLine(); // Consumir el salto de línea
+            encontrado = buscarPorCodigo(codigo, listaArticulos);
+        } else if (criterioBusqueda == 2) {
+            System.out.println("Ingrese el nombre del artículo:");
+            String nombre = scanner.nextLine();
+            encontrado = buscarPorNombre(nombre, listaArticulos);
+        }
+        if (encontrado != null) {
+            System.out.println("Artículo encontrado: " + encontrado);
+
+            // Presentar opciones al usuario
+            System.out.println("Seleccione una opción:");
+            System.out.println("1. Modificar");
+            System.out.println("2. Eliminar");
+            System.out.println("3. No hacer nada");
+            int eleccion = scanner.nextInt();
+
+            switch (eleccion) {
+                case 1:
+                    modificar(encontrado); //Le paso "encontrado" que es el articulo a modificar
+                    break;
+                case 2:
+                    // Llamada a una función para eliminar (a implementar)
+                    break;
+                case 3:
+                    // No hacer nada
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        } else {
+            System.out.println("No se encontró el artículo con el código proporcionado.");
+        }
     }
     
-    
-    private JSONObject buscarEnLista(int codigo, JSONArray lista) {
+    private JSONObject buscarPorCodigo(int codigo, JSONArray lista) {
         for (Object item : lista) {
             JSONObject obj = (JSONObject) item;
-            if (((Long) obj.get("Codigo")).intValue() == codigo) {
+            if (((Number) obj.get("Codigo")).intValue() == codigo) {
+                return obj;
+            }
+        }
+        return null;
+    }
+    
+    private JSONObject buscarPorNombre(String nombre, JSONArray lista) {
+        for (Object item : lista) {
+            JSONObject obj = (JSONObject) item;
+            if (obj.get("Nombre").toString().equalsIgnoreCase(nombre)) {
                 return obj;
             }
         }
         return null;
     }
 
-    public void buscar() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("¿Qué desea buscar?");
-        System.out.println("1. Producto");
-        System.out.println("2. Artículo");
-        int tipoBusqueda = scanner.nextInt();
-        scanner.nextLine(); // Consumir el salto de línea
-
-        System.out.println("Ingrese el código del producto o artículo:");
-        int codigo = scanner.nextInt();
-        scanner.nextLine(); // Consumir el salto de línea
-
-        JSONObject encontrado = null;
-
-        if (tipoBusqueda == 1) {
-            encontrado = buscarEnLista(codigo, listaProductos);
-            if (encontrado != null) {
-                System.out.println("Producto encontrado: " + encontrado);
-            }
-        } else if (tipoBusqueda == 2) {
-            encontrado = buscarEnLista(codigo, listaArticulos);
-            if (encontrado != null) {
-                System.out.println("Artículo encontrado: " + encontrado);
-            }
-        }
-
-        if (encontrado == null) {
-            System.out.println("No se encontró el producto o artículo con el código proporcionado.");
-            return;
-        }
-
-        // Presentar opciones al usuario
-        System.out.println("Seleccione una opción:");
-        System.out.println("1. Modificar");
-        System.out.println("2. Eliminar");
-        System.out.println("3. No hacer nada");
-        int eleccion = scanner.nextInt();
-
-        switch (eleccion) {
-            case 1:
-                modificar(encontrado);
-                break;
-            case 2:
-                // Llamada a una función para eliminar (no implementada en este ejemplo)
-                break;
-            case 3:
-                // No hacer nada
-                break;
-            default:
-                System.out.println("Opción no válida.");
-        }
-    }
-    
-
+   
 
     public static void main(String[] args) {
         RegistroProductos registro = new RegistroProductos();
@@ -280,25 +253,21 @@ public class RegistroProductos {
 
         while (true) {
             System.out.println("Seleccione una opción:");
-            System.out.println("1. Agregar Producto");
-            System.out.println("2. Agregar Artículo");
-            System.out.println("3. Buscar Producto/Artículo");
-            System.out.println("4. Salir");
+            System.out.println("1. Agregar Artículo");
+            System.out.println("2. Buscar Artículo");
+            System.out.println("3. Salir");
             System.out.print("Ingrese su opción: ");
 
             int eleccion = scanner.nextInt();
 
             switch (eleccion) {
                 case 1:
-                    registro.agregarProducto();
-                    break;
-                case 2:
                     registro.agregarArticulo();
                     break;
-                case 3:
+                case 2:
                     registro.buscar();
                     break;
-                case 4:
+                case 3:
                     System.out.println("Saliendo...");
                     return;
                 default:
@@ -306,5 +275,4 @@ public class RegistroProductos {
             }
         }
     }
-
 }
