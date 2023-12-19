57979024a7cd86028fc2204df11e679b0b42be66
Norman Sáenz A <saenznorman73@gmail.com>
2023-12-18 23:22:33 -0600

    Primera fase de Mantenimiento

--- grupoJeffMelanieNorman/src/main/java/com/mycompany/grupojeffmelanienorman/Mantenimiento.java
+++ grupoJeffMelanieNorman/src/main/java/com/mycompany/grupojeffmelanienorman/Mantenimiento.java
@@ -1,33 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.grupojeffmelanienorman;
 
-/**
- *
- * @author XPC
- */
-public class Mantenimiento {
-    // Atributos
-    private int idMantenimiento;
-    private int idCliente;
-    private int precio;
-    private String descripcion;
-    private String fechaSalida;
-    private String fechaIngreso;
-    private boolean estado = false;
-    private String marca;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
-    // Constructor
-//    public Mantenimiento( int idCliente, int precio, String descripcion, String fechaSalida, String fechaIngreso, boolean estado, String marca) {
-//        this.idMantenimiento = idMantenimiento;
-//        this.idCliente = idCliente;
-//        this.precio = precio;
-//        this.descripcion = descripcion;
-//        this.fechaSalida = fechaSalida;
-//        this.fechaIngreso = fechaIngreso;
-//        this.estado = estado;
-//        this.marca = marca;
-//    }
+public class Mantenimiento {
+
+    private JSONArray listaServicios;
+    private int ultimoCodigoServicio;
+    private static final String FILE_PATH = "Mantenimientos.json";
+
+    public Mantenimiento() {
+        cargarDatos();
+    }
+
+    private void cargarDatos() {
+        JSONParser parser = new JSONParser();
+        try {
+            FileReader reader = new FileReader(FILE_PATH);
+            JSONObject obj = (JSONObject) parser.parse(reader);
+            listaServicios = (JSONArray) obj.getOrDefault("Servicios", new JSONArray());
+            actualizarUltimoCodigo();
+        } catch (Exception e) {
+            listaServicios = new JSONArray();
+        }
+    }
+
+    private void guardarDatos() {
+        try (FileWriter file = new FileWriter(FILE_PATH)) {
+            JSONObject obj = new JSONObject();
+            obj.put("Servicios", listaServicios);
+            file.write(obj.toJSONString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void actualizarUltimoCodigo() {
+        if (!listaServicios.isEmpty()) {
+            JSONObject ultimoServicio = (JSONObject) listaServicios.get(listaServicios.size() - 1);
+            ultimoCodigoServicio = ((Long) ultimoServicio.get("Codigo")).intValue() + 1;
+        } else {
+            ultimoCodigoServicio = 1;
+        }
+    }
+
+    public JSONObject agregarServicio(String marcaBicicleta, String descripcionBicicleta, int precio) {
+        JSONObject nuevoServicio = new JSONObject();
+        nuevoServicio.put("Codigo", ultimoCodigoServicio++);
+        nuevoServicio.put("Marca Bicicleta", marcaBicicleta.trim());
+        nuevoServicio.put("Descripcion Bicicleta", descripcionBicicleta.trim());
+        nuevoServicio.put("Precio", precio);
+
+        listaServicios.add(nuevoServicio);
+        guardarDatos();
+
+        return nuevoServicio;
+    }
 }
+
