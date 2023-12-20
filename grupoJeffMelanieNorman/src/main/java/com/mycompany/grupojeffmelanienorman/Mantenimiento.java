271fab208b55f2cb48eb48f0860801057ee084d6
Norman Sáenz A <saenznorman73@gmail.com>
2023-12-19 20:49:39 -0600

    Update Mantenimiento.java

--- grupoJeffMelanieNorman/src/main/java/com/mycompany/grupojeffmelanienorman/Mantenimiento.java
+++ grupoJeffMelanieNorman/src/main/java/com/mycompany/grupojeffmelanienorman/Mantenimiento.java
@@ -136,7 +136,7 @@ public class Mantenimiento {
         return encontrado;
     }
 
-    public void modificarServicio(JSONObject servicio, String marcaBicicleta, String descripcionBicicleta, int precio, String fechaRecibido, String fechaEntrega, String observaciones, String estado) {
+    public void modificarServicios(JSONObject servicio, String marcaBicicleta, String descripcionBicicleta, int precio, String fechaRecibido, String fechaEntrega, String observaciones, String estado) {
         servicio.put("Marca Bicicleta", marcaBicicleta.trim());
         servicio.put("Descripcion Bicicleta", descripcionBicicleta.trim());
         servicio.put("Precio", precio);
@@ -149,7 +149,7 @@ public class Mantenimiento {
     }
     
     
-    public void eliminarServicio(JSONObject servicio) {
+    public void eliminarServicios(JSONObject servicio) {
         listaServicios.remove(servicio);
         guardarDatos();
     }
